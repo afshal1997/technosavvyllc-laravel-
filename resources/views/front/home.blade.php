2b1eb7db7d8adb1235bb9f568aa4693c3f73b7c6
Afshal Warsi <afshal.warsi@shottech.co>
2022-06-17 15:01:07 -0700

    remaining pages

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -12,7 +12,9 @@
                         <div class="container">
                             <div class="row sam-logo-ban">
                                 <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12 first_col" data-aos="">
-                                    <h1 class="h2-heading white-color mb-xs-10"><strong>A scintillating retort to the digital question mark!</strong></h1>
+                                    <h1 class="h2-heading white-color mb-xs-10">
+                                        <strong>A scintillating retort to the digital question mark!</strong>
+                                    </h1>
                                     <h3>
                                         Dignify & digitalize the countenance of your inestimable business by perpetuating its digital trail, engraving its achievements, and augmenting your ROI by choosing the comprehensive Government affiliated solution.
                                     </h3>
